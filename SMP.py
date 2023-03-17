dacbc4d05eee7f4d68c5a26eb58b2812f6136a60
maukthik05 <128088125+maukthik05@users.noreply.github.com>
2023-03-18 05:07:18 +0530

    Update SMP.py

--- SMP.py
+++ SMP.py
@@ -44,7 +44,7 @@ if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
-    st.image(image, use_column_width=True)
     predictions = import_and_predict(image, model)
     st.write(
-    "This soil has a moisture content of",predictions)
+    "This soil has a moisture content of",predictions[0][0])
+    st.image(image, use_column_width=True)
